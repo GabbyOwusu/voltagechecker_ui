4ddf64f1cc322d7c02f1dcac8a543ec2549f3730
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-06-13 15:10:48 +0100

    Added New Model

---
+++ lib/models/roomDetails.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class RoomDetails {
+  String icon;
+  Color iconColor;
+  Color color;
+  String text;
+  Color textColor;
+
+  RoomDetails({
+    this.icon,
+    this.color,
+    this.iconColor,
+    this.text,
+    this.textColor,
+  });
+}
