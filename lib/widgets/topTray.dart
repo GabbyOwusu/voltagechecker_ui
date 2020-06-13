614b7038d776aac2f074f5e8d17a4f2028f2f913
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-06-13 15:12:05 +0100

    A New Custom Widget

--- lib/widgets/topTray.dart
+++ lib/widgets/topTray.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:new_ui/models/roomDetails.dart';
 
 class TopTray extends StatelessWidget {
- 
+  final RoomDetails roomdetails;
+  TopTray({
+    this.roomdetails,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      child: ListView.separated(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) => Column(
-                children: <Widget>[
-                  Container(
-                    height: 70,
-                    width: 70,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(color: Colors.grey, width: 1)),
-                  ),
-                  SizedBox(height: 10),
-                  Text('data')
-                ],
+    return Column(
+      children: <Widget>[
+        Container(
+            height: 70,
+            width: 70,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: roomdetails.color,
+              border: Border.all(color: Colors.grey, width: 1),
+            ),
+            child: Center(
+              child: Image(
+                image: AssetImage(
+        roomdetails.icon,
+                ),
+                width: 30, color: roomdetails.iconColor,
               ),
-          separatorBuilder: (context, index) => SizedBox(width: 25),
-          itemCount: 10),
+            ),
+          ),
+        SizedBox(height: 10),
+        Text(roomdetails.text, style: TextStyle(color: roomdetails.textColor),)
+      ],
     );
   }
 }
